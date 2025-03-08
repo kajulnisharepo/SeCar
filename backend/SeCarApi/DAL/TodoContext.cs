01a1e23b23ad466aac456cfd267693dc482b4e7c
Kajul Nisha <v-kumarsharif@extendedstay.com>
2025-03-08 17:29:09 +0530

    Initial commit

---
+++ backend/SeCarApi/DAL/TodoContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SeCarApi.Models;
+
+namespace SeCar;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+}
